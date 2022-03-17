0ba54aa7eaff05e62d858b6f36c8da637e3886a0
unknown <yshaggy@heelmail.unc.edu>
2022-03-16 20:55:09 -0400

    changed last print

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
     server.listen(port, () => {
-        console.log('Server running at port ${port}')
+        console.log('Server running at port ' + port)
     })
 })
 
